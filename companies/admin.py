1c74c2926abf6bca8b10b0700f8c64e9e5f00be1
buthontingtimothy <buthontingtimothy@gmail.com>
2026-01-14 00:55:32 +0800

    companies model and admin revert

--- companies/admin.py
+++ companies/admin.py
@@ -4,10 +4,9 @@ from .models import Company
 # Register your models here.
 class CompanyAdmin(admin.ModelAdmin):
 
-    list_display = 'industry', 'title', 'size', 'contact_name', 'register_date',
-    list_display_links = 'title',
+    list_display = 'name', 'email', 'industry', 'create_date',
+    list_display_links = 'name', 'email',
     list_editable = 'industry',
-    search_fields = 'industry', 'title', 'size', 'description', 'contact_name', 'register_date',
-    list_per_page = 25
+    search_fields = 'name',
 
 admin.site.register(Company, CompanyAdmin)
--- companies/models.py
+++ companies/models.py
@@ -2,16 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Company(models.Model):
-    title = models.CharField(max_length=200)
-    industry = models.CharField(max_length=50)
+    name= models.CharField(max_length=200)
     logo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    size = models.CharField(max_length=20) 
     description = models.TextField(blank=True)
-    serivces = models.TextField(blank=True)
-    contact_name = models.CharField(max_length=200)
-    contact_email = models.EmailField(max_length=50, unique=True, blank=False)
-    contact_phone = models.CharField(max_length=20, blank='00000000')
-    register_date = models.DateTimeField(auto_now_add=True)
+    Serivces = models.TextField(blank=True)
+    phone = models.CharField(max_length=20, blank='00000000')
+    email = models.EmailField(max_length=50, unique=True, blank=False)
+    industry = models.CharField(max_length=50)
+    create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.name
