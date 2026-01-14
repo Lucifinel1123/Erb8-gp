89cdffd57c25379b403c986262fb7136a2a5f8f5
buthontingtimothy <buthontingtimothy@gmail.com>
2026-01-14 17:43:30 +0800

    listings with card

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Listing
 
 # Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = 'industry', 'title', 'budget', 'company', 'publish_date', 'is_active',
+    list_display_links = 'industry', 'title', 'company',
+    list_editable = 'is_active',
+    search_fields = 'industry', 'title', 'budget', 'company', 'publish_date', 'is_active',
+    list_per_page = 50
+
+admin.site.register(Listing, ListingAdmin)
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from .models import Listing
+from .choices import industry_choices, budget_choices
+from django.db.models import Q
 
 # Create your views here.
 # def listings(request):
@@ -9,14 +12,61 @@ from django.shortcuts import render, HttpResponse
     # context = {"listings": paged_listings}
     # return render(request, 'listings/listings.html', 
                 #   context
-                  # )
+                # )
     # return HttpResponse("<h1>about</h1>")
 
 def listings(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.filter(is_active=True)
+    context = {
+        "listings": listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {"listing": listing}
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=keywords)|
+                Q(company__name__icontains=keywords)|
+                Q(description__icontains=keywords)|
+                Q(requirement__icontains=keywords)
+                )
+    if 'industry' in request.GET:
+        industry = request.GET['industry']
+        if industry:
+            queryset_list = queryset_list.filter(
+                Q(industry__iexact=industry)
+            )
+    if 'budget' in request.GET:
+        budget = request.GET['budget']
+        if budget:
+            queryset_list = queryset_list.filter(
+                Q(budget__gte=budget)
+            )
+    if 'duration' in request.GET:
+        duration = request.GET['duration']
+        if duration:
+            queryset_list = queryset_list.filter(
+                Q(duration__lte=duration)
+            )
+    if 'publish_date' in request.GET:
+        publish_date = request.GET['publish_date']
+        if publish_date:
+            queryset_list = queryset_list.filter(
+                Q(publish_date__gte=publish_date)
+            )
+
+    context = {
+        'listings': queryset_list,
+        'industry_choices': industry_choices,
+        'budget_choices': budget_choices,
+    }
+
+    return render(request, 'listings/search.html', context)
