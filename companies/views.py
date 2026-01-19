4697957412cb72985da8e732970feaa859225877
Lucifinel1123 <lam201123@gmail.com>
2026-01-19 15:44:06 +0800

    apply job finish

--- companies/views.py
+++ companies/views.py
@@ -1,27 +1,68 @@
 from django.shortcuts import render,redirect, get_object_or_404, HttpResponse
 from .models import Company
+from listings.models import Listing
+from applies.models import Apply
+#from .forms import CompanyInfo, Job_post
+#from django.shortcuts import login_required
 #from django.contrib import messages
 
 # Create your views here.
-def company(request):
+def company(request,company_id):
+    company = get_object_or_404(Company, pk = company_id)
+    context = {'company':company}
+    return render(request,'companies/company.html', context)
+
+
+def HR_dashboard(request):
+    try:
+        company = get_object_or_404(Company, user=request.user.id)
+        job_posts = Listing.objects.all().filter(company = company).order_by('-publish_date')
+        context = {"job_post": job_posts}
+        return render(request, 'companies/HR_dashboard.html', context)
+    except:
+        return render(request, 'companies/HR_dashboard.html')
+
+#@login_required
+def company_edit_info(request):
     if request.method=='POST':
-        company_id = request.POST['company_id']
+        user_id = request.POST['user_id']
         name = request.POST['name']
         email = request.POST['email']
         phone = request.POST['phone']
-        company = Company(company_id=company_id, name=name, 
-                        email=email, phone=phone)
+        industry = request.POST['industry']
+        serivces = request.POST['serivces']
+        description = request.POST['description']
+        company = Company(user_id=user_id, name=name, 
+                        email=email, phone=phone, industry=industry,
+                        serivces=serivces, description=description)
         company.save()
-        return redirect('companies:company', company_id = company_id)
-    return HttpResponse("<h1>company</h1>")
+        return render(request, 'companies/HR_dashboard.html')  
+    else:
+        return render(request, 'companies/company_edit_info.html')
 
-def HR_dashboard(request):
-    '''
-    job_post = Listing.objects.all().filter()
-    user_appliers = Apply.objects.all().filter(
-        user_id = request.user.id).order_by('-contact_date')
+def job_post(request):
+    if request.method=='POST':
+        user_id = request.POST['user_id']
+        company = get_object_or_404(Company, user_id=user_id)
+        title = request.POST['title']
+        industry = request.POST['industry']
+        budget = request.POST['budget']
+        duration = request.POST['duration']
+        requirement = request.POST['requirement']
+        description = request.POST['description']
+        listing = Listing(title=title, company=company,
+                        industry=industry, budget=budget, duration=duration,
+                        requirement=requirement, description=description)
+        listing.save()
+        context = {"company": company}
+        return render(request, 'companies/HR_dashboard.html',context)
+    else:
+        return render(request,'companies/job_post.html')
     
-    context = {"job_post": job_post, "user_appliers": user_appliers}
-    '''
-    return render(request, 'companies/HR_dashboard.html')
 
+def view_candidate(request, listing_id):
+    listing_id = request.POST['listing_id']
+    user_appliers = Apply.objects.all().filter(
+        listing_id = listing_id).order_by('-apply_date')
+    context = {"user_appliers":user_appliers}
+    return render(request, 'companies/view_candidate.html',context)
--- listings/models.py
+++ listings/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from companies.models import Company
+from companies.models import Company 
 # Create your models here.
 
 class Listing(models.Model):
-    industry = models.CharField(max_length=50)
-    title = models.CharField(max_length=200)
-    budget = models.CharField(max_length=50)
     company = models.ForeignKey(Company, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    industry = models.CharField(max_length=50)
+    budget = models.CharField(max_length=50)
     duration = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     requirement = models.TextField(blank=True)
@@ -15,12 +15,4 @@ class Listing(models.Model):
     
     def __str__(self):
         return self.title
-
-
-
-
-
-
-
-
-
+    
