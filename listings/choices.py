3d9509715d9f1cecec89ba22345824c4490f4b13
buthontingtimothy <buthontingtimothy@gmail.com>
2026-01-22 16:50:56 +0800

    choices.py update

--- listings/choices.py
+++ listings/choices.py
@@ -1,20 +1,22 @@
 industry_choices = {
-    "Programming": "Programming",
-    "Design": "Design",
-    "Content Writing": "Content Writing",
-}
+    'Technology': 'Technology', 'Finance': 'Finance', 'Healthcare': 'Healthcare', 'Education': 'Education', 'Marketing': 'Marketing', 'Retail': 'Retail', 'Manufacturing': 'Manufacturing', 'Real Estate': 'Real Estate', 'Hospitality': 'Hospitality', 'Construction': 'Construction', 'Transportation': 'Transportation', 'Entertainment': 'Entertainment', 'Agriculture': 'Agriculture', 'Energy': 'Energy', 'Telecommunications': 'Telecommunications', 'Consulting': 'Consulting', 'Legal': 'Legal', 'Non-Profit': 'Non-Profit', 'Government': 'Government', 'Media': 'Media'}
 
-budget_choices = {
-    "<$1000": "<$1000",
-    "$1000-$3000": "$1000-$3000",
-    "$3000-$5000": "$3000-$5000",
-    ">$5000": ">$5000",
-}
+budget_choices = {'$500-$1,000': '$500-$1,000', '$1,000-$2,000': '$1,000-$2,000', '$2,000-$5,000': '$2,000-$5,000', '$5,000-$10,000': '$5,000-$10,000', '$10,000-$20,000': '$10,000-$20,000', '$20,000-$50,000': '$20,000-$50,000', '$50,000+': '$50,000+', 'Negotiable': 'Negotiable', 'Hourly Rate': 'Hourly Rate'}
 
-duration_choices = {
-    "<1 week": "<1 week",
-    "1-2 week(s)": "1-2 week(s)",
-    "2-3 weeks": "2-3 weeks",
-    "3-4 weeks": "3-4 weeks",
-    ">4 weeks": ">4 weeks",
-}
+# {
+#     "<$1000": "<$1000",
+#     "$1000-$3000": "$1000-$3000",
+#     "$3000-$5000": "$3000-$5000",
+#     ">$5000": ">$5000",
+# }
+
+
+duration_choices = {'1-2 weeks': '1-2 weeks', '3-4 weeks': '3-4 weeks', '1-2 months': '1-2 months', '3-4 months': '3-4 months', '5-6 months': '5-6 months', '7-12 months': '7-12 months', '1+ year': '1+ year', 'Ongoing': 'Ongoing', 'Project-based': 'Project-based'}
+
+# {
+#     "<1 week": "<1 week",
+#     "1-2 week(s)": "1-2 week(s)",
+#     "2-3 weeks": "2-3 weeks",
+#     "3-4 weeks": "3-4 weeks",
+#     ">4 weeks": ">4 weeks",
+# }
